2f8abeca464982004ded2e12db718b314c37a4d4
zhur4k <kostya.zhurovich12052@gmail.com>
2024-12-08 11:16:58 +0300

    feat:initial project.

---
+++ src/main/java/com/streamapi/Main.java
@@ -0,0 +1,16 @@
+package com.streamapi;
+
+import java.util.concurrent.ForkJoinPool;
+
+public class Main {
+    public static void main(String[] args) {
+        int n = 10; // Вычисление факториала для числа 10
+
+        ForkJoinPool forkJoinPool = new ForkJoinPool();
+        FactorialTask factorialTask = new FactorialTask(n);
+
+        long result = forkJoinPool.invoke(factorialTask);
+
+        System.out.println("Факториал " + n + "! = " + result);
+    }
+}
